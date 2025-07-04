e6285e0b715ae90152b82c1980769a4d4452b89c
JMac-stack <jamesmclauchlan2@live.co.uk>
2025-07-04 14:28:19 +0100

    Completed Assignment
    Completed Assignment. Created simple console app.

---
+++ Assignment_Console_App/Assignment_Console_App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace C__Assigment_Console.cs
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
+
