4f775ee2156948d304cafcd84550ec5a50e49faf
JMac-stack <jamesmclauchlan2@live.co.uk>
2025-07-31 10:41:32 +0100

    progress

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num1 = 8.9563m;
+            sbyte num2 = 9;
+
+            sbyte total = num1 + num2;
+
+            Console.WriteLine(total);
+        }
+    }
+}
