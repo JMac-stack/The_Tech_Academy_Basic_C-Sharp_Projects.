1687ad355c4f1054ca3c211132a8ad5ef6a207ac
JMac-stack <jamesmclauchlan2@live.co.uk>
2025-08-01 14:05:51 +0100

    Complete.

---
+++ ConsoleApp_Strings_Ints_Assignment/ConsoleApp_Strings_Ints_Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp_Strings_Ints_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 100, 54, 90, 50, 200, 10 }; // Create a list of integers.
+
+            try
+            {
+                Console.WriteLine("Please enter a number to divide by.");
+                int inputNumber = Convert.ToInt32(Console.ReadLine());
+                int outputNumber;
+                foreach (int number in numbers) // Loop to divide numbers by input.
+                {
+                    outputNumber = number / inputNumber;
+                    Console.WriteLine($"{number} divided by {inputNumber} equals {outputNumber}");
+                }
+            }
+            catch (DivideByZeroException ex) // Catches divide by zero exceptions.
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (FormatException ex) // Catches when wrong format entered.
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please enter a whole number.");
+            }
+            catch (Exception ex) // Generic Exception.
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            Console.WriteLine("Try/catch Complete."); // Code to run after try/catch block
+        }
+    }
+}
