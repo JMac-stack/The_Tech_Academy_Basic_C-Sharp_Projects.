efa0c856f3c3c657e030056536f1f808d391096f
JMac-stack <jamesmclauchlan2@live.co.uk>
2025-08-07 12:56:22 +0100

    Complete Main Method Assignment.

---
+++ Main_Method_Assignment/Main_Method_Assignment/Maths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Main_Method_Assignment
+{
+    internal class Maths // Create a class.
+    {
+
+        public int MathsOp(int x) // In that class, create a method that will take in an integer, create a math operation for this integer (addition, subtraction, etc.), then return the answer as an integer.
+        {
+            return x + 2;
+        }
+
+        public decimal MathsOp(decimal x) // Add a second method to the class with the same name that will take in a decimal, create a different math operation for it, then return the answer as an integer.
+        {
+            return x - 2.5m;
+        }
+
+        public int MathsOp(string x)
+        {
+            return Convert.ToInt32(x) * 5; //Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it, then return the answer as an integer.
+        }
+    }
+}
---
+++ Main_Method_Assignment/Main_Method_Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Main_Method_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths mathsObject = new Maths();
+            Console.WriteLine(mathsObject.MathsOp(5)); //In the Main() method of the console app, instantiate the class and call the one method, passing in an integer. Display the result to the screen.
+
+            Console.WriteLine(mathsObject.MathsOp(27.75m)); //call the second method, passing in a decimal. Display the result to the screen.
+
+            Console.WriteLine(mathsObject.MathsOp("101")); // call the third method, passing in a string that equates to an integer. Display the result to the screen.
+        }
+    }
+}
