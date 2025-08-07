c7c89bc881b49c850a6e8e9eb9fa8f68d2449fc9
JMac-stack <jamesmclauchlan2@live.co.uk>
2025-08-07 17:24:23 +0100

    Complete Method Class Assignment Submission.

---
+++ Method_Class_Assignment/Method_Class_Assignment/Method_Class_Assignment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Class_Assignment
+{
+    internal class Method_Class_Assignment // Create a class. In that class, create a void method that takes two integers as parameters.
+    {
+        public void takeTwoInt(int firstNum, int secondNum)
+        {
+            firstNum = firstNum * 10; // Maths operation on first int.
+
+            Console.WriteLine(secondNum); // Display second int on screen.
+        }
+    }
+}
---
+++ Method_Class_Assignment/Method_Class_Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Method_Class_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Method_Class_Assignment methodObject = new Method_Class_Assignment(); // Instantiate the class.
+            methodObject.takeTwoInt(5, 10); // Call the method in the class, passing in two numbers.
+            methodObject.takeTwoInt(firstNum: 33, secondNum: 55); // Call the method in the class, specifying the parameters by name.
+        }
+    }
+}
