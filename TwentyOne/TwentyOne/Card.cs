79d3d526adec806dabb77b9cd8fba9a35a92bbf5
JMac-stack <jamesmclauchlan2@live.co.uk>
2025-08-08 12:25:16 +0100

    Renamed project.

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public Card()
+        {
+        }
+        public string Suit {  get; set; }
+        public string Face { get; set; }
+    }
+}
---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwentyOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string> { "Jesse", "Bill", "Joe" };
+            game.ListPlayers();
+            game.Play();
+            Deck deck = new Deck();
+            deck.Shuffle(times: 1);
+            deck.ViewCards(); // Displays cards of deck on screen in order.
+
+        }
+    }
+}
