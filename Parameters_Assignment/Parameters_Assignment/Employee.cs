1d11fca9d2a0d235b61c7a651721b5d20fc672b2
JMac-stack <jamesmclauchlan2@live.co.uk>
2025-08-09 13:45:27 +0100

    Parameters Assignment Complete.

---
+++ Parameters_Assignment/Parameters_Assignment/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods_and_Objects_Assignment
+{
+    public class Employee<T> : Person // Inherits from person class + generic parameter..
+    {
+        public int Id { get; set; }
+        public List<T> Things { get; set; } // Add a property to the Employee class called “things” and have its data type be a generic list matching the generic type of the class.
+        public Employee(string firstName, string lastName, int id) : base(firstName, lastName) // Constructor uses person class instructor + new property.
+        {
+            this.Id = id;
+            this.Things = new List<T>(); // Instantiates List, matches generic type of class.
+        }
+    }
+}
---
+++ Parameters_Assignment/Parameters_Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Methods_and_Objects_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object with type “string” as its generic parameter. Assign a list of strings as the property value of Things.
+            Employee<string> employeeWithStrings = new Employee<string>("Test", "String", 1);
+            employeeWithStrings.Things.Add("string1");
+            employeeWithStrings.Things.Add("string2");
+            employeeWithStrings.Things.Add("string3");
+
+            // Instantiate an Employee object with type “int” as its generic parameter. Assign a list of integers as the property value of Things.
+            Employee<int> employeeWithInts = new Employee<int>("Test", "Int", 2);
+            employeeWithInts.Things.Add(1);
+            employeeWithInts.Things.Add(3);
+            employeeWithInts.Things.Add(5);
+            employeeWithInts.Things.Add(7);
+
+            // Create a loop that prints all of the Things to the Console.
+            foreach (string str in employeeWithStrings.Things)
+            {
+                Console.WriteLine(str);
+            }
+            foreach (int i in employeeWithInts.Things)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
