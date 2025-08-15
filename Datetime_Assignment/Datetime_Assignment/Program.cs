de3999c377a62f8d6a2ceb6cc68477ebde4b5793
JMac-stack <jamesmclauchlan2@live.co.uk>
2025-08-15 13:49:59 +0100

    Datetime Assignment Complete.

---
+++ Datetime_Assignment/Datetime_Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Datetime_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime nowTime = DateTime.Now;
+            Console.WriteLine("The time is now: {0}",nowTime); // Prints the current date and time to the console.
+            Console.WriteLine("Please enter a number."); // Asks the user for a number.
+            int num = Convert.ToInt32(Console.ReadLine());
+            TimeSpan hours = new TimeSpan(num,0,0);
+            Console.WriteLine("In {0} hours it will be {1}",hours.Hours,(nowTime.Add(hours))); // Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+        }
+    }
+}
--- TwentyOne/TwentyOne/Dealer.cs
+++ TwentyOne/TwentyOne/Dealer.cs
@@ -19,6 +19,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\james\logs\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
