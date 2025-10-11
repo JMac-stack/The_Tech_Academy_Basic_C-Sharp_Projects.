e14d6cd430c2599949e6f5f3b92b04b4a41fa8a7
JMac-stack <jamesmclauchlan2@live.co.uk>
2025-10-11 15:18:46 +0100

    Finished

---
+++ CarInsurance/CarInsurance/Models/InsureeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsurance.Models
+{
+    public class InsureeViewModel
+    {
+        public InsureeViewModel(string firstname, string lastname, string emailaddress, decimal quote)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            EmailAddress = emailaddress;
+            Quote = quote;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal Quote { get; set; }
+    }
+}
